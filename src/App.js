84b31a22717043414fca78ed578392cbfa7a529e
richardlaiis <richardlaiis@proton.me>
2024-07-14 13:51:25 +0800

    props test

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
           </div>
         </div>
       </div>
-      <CardFooter />
+      <CardFooter inputValue={inputValue}/>
     </div>
   );
 }
--- src/conponents/cardFooter.js
+++ src/conponents/cardFooter.js
@@ -1,2 +1,36 @@
-const CardFooter = () => <div className="card-footer">FAST</div>;
+const CardFooter = (props) => {
+    const { inputValue } = props;
+    let criteria = {};
+    if (!inputValue) {
+        criteria = {
+            title: '===',
+            backgroundColor: '#DBE2EF',
+        };
+    } else if (inputValue < 15) {
+        criteria = {
+            title: 'SLOW',
+            backgroundColor: '#FFB4C2',
+        };
+    } else if ( inputValue < 40) {
+        criteria = {
+            title: 'MEDIUM',
+            backgroundColor: '#667BC6',
+        };
+    } else {
+        criteria = {
+            title: 'FAST',
+            backgroundColor: '#91DDCF',
+        };
+    }
+    return (
+        <div
+            className="card-footer"
+            style={{
+                backgroundColor: criteria.backgroundColor,
+            }}
+        >
+            {criteria.title}
+        </div>
+    );
+}
 export default CardFooter;
